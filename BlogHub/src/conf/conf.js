b76223bdee1b420b070ca63bfc3627f6b3dd3c57
Abinash Bhutia <abinashbhutia78@gmail.com>
2026-08-13 10:47:31 +0530

    ccreate services

---
+++ BlogHub/src/conf/conf.js
@@ -0,0 +1,13 @@
+// Environment variables are read as strings by default.
+// there we converts all the 'env' variables into string 
+// import.meta.env. => it is the syntax for access the 'env' variables
+
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
